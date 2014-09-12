f118bc9702e49bb3442339a2bb8f5f9621feb411
mooreds <github@mooreds.com>
2014-09-12 08:03:04 -0600

    added routing

--- app/js/app.js
+++ app/js/app.js
@@ -5,3 +5,19 @@ var dealsApp = angular.module('dealApp', [
 'dealControllers',
 'dealServices'
 ]);
+
+dealsApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/deals', {
+        templateUrl: 'partials/deal-list.html',
+        controller: 'ListCtrl'
+      }).
+      //when('/pulses/:pulseId', {
+        //templateUrl: 'partials/pulse-detail.html',
+        //controller: 'PulseDetailCtrl'
+      //}).
+      otherwise({redirectTo: 'deals'});
+   }
+]);
+
