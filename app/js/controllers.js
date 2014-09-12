2b0f6a3c29b39c495edd04a2358b46c8d5145a5e
mooreds <github@mooreds.com>
2014-09-12 07:49:27 -0600

    added real life data to tests

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,14 @@
 /* Controllers Module */
 var dealsControllers = angular.module('dealControllers', []);
 
-dealsControllers.controller('ListCtrl', ['$scope', 'Deals', function($scope, Deal) {
-  $scope.deals = Deal.query();
-  $scope.orderProp = 'deadline';
+dealsControllers.controller('ListCtrl', ['$scope', 'Deals', function(sc,d) {
+  var deals = [];
+  sc.orderProp = 'deadline';
+  var oneWeekAgo = new Date();
+  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+  var fmtdate = oneWeekAgo.getFullYear()+'-'+(oneWeekAgo.getMonth()+1)+'-'+oneWeekAgo.getDate();
+  sc.searchDate = sc.searchDate || fmtdate;
+  d.query(sc.searchDate).then(function(deals) {
+     sc.deals = deals;
+  });
 }]);
--- app/js/services.js
+++ app/js/services.js
@@ -2,28 +2,17 @@
 
 var dealServices = angular.module('dealServices',[]);
 
-dealServices.factory('Deals',['$http',function($http,$scope) {
-  var url = "http://api.remix.bestbuy.com/v1/products(offers.startDate>2014-08-26)?show=name,sku,image,url,offers.type,offers.url,offers.text&pageSize=100&format=json&apiKey=zsgty2qhg93qstyadx88yruf";
+dealServices.factory('Deals',['$http',function(http) {
+  var url = "http://api.remix.bestbuy.com/v1/products(offers.startDate>XXXSTARTDATEXXX)?show=name,sku,image,url,offers.type,offers.url,offers.text&pageSize=100&format=json&apiKey=zsgty2qhg93qstyadx88yruf";
+   
    var Deals = function(data) {
         angular.extend(this, data);
     };
-  Deals.get = function() {
-    $http.get(url).success(function(data,status){
-      $scope.data = data;
-      $scope.status = status;
-    }).error(function(data,status){
-      $scope.data = data || "request failed";
-      $scope.status = status;
-    });
-  }
-  Deals.query = function() {
-    $http.get(url).success(function(data,status){
-      $scope.data = data;
-      $scope.status = status;
-    }).error(function(data,status){
-      $scope.data = data || "request failed";
-      $scope.status = status;
-    });
+  Deals.query = function(startdate) {
+    url = url.replace("XXXSTARTDATEXXX",startdate);
+    return http.get(url).then(function (response) {
+      return response.data.products;
+     });
   }
   return Deals;
 }]);
